32d6befb5d6738c4912fce88eda902e171716c2b
abhi01820 <johnabhi744@gmail.com>
2025-06-06 16:07:20 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,59 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import Sidebar from "../components/Profile/Sidebar";
-// import { Outlet } from "react-router-dom";
-// import axios from "axios";
-// import Loader from "../components/Loader/Loader";
-// import MobileNav from "../components/Profile/MobileNav";
 
-// const Profile = () => {
-//   const [Profile, setProfile] = useState();
-//   const headers = {
-//     id: localStorage.getItem("id"),
-//     authorization: `Bearer ${localStorage.getItem("token")}`,
-//   };
-
-//   useEffect(() => {
-//     const fetch = async () => {
-//       try {
-//         const response = await axios.get(
-//           "http://localhost:1000/api/v1/get-user-info",
-//           { headers }
-//         );
-//         setProfile(response.data);
-//       } catch (error) {
-//         console.error(
-//           "Error fetching user info:",
-//           error.response?.data || error
-//         );
-//       }
-//     };
-
-//     fetch();
-//   }, []);
-
-//   return (
-//     <div className="bg-zinc-900 px-2 md:px-12 flex flex-col md:flex-row h-auto py-8 gap-4 text-white">
-//       {!Profile && (
-//         <div className="w-full h-[100%] flex items-center justify-center">
-//           <Loader />
-//         </div>
-//       )}
-//       {Profile && (
-//         <>
-//           <div className="w-full   md:w-1/6">
-//             <Sidebar data={Profile} />
-//             <MobileNav />
-//           </div>
-//           <div className="w-full p-10 h-auto lg:h-screen  md:w-5/6">
-//             <Outlet />
-//           </div>
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Profile;
 import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Profile/Sidebar";
 import { Outlet } from "react-router-dom";
@@ -61,6 +6,8 @@ import axios from "axios";
 import Loader from "../components/Loader/Loader";
 import MobileNav from "../components/Profile/MobileNav";
 import { useSelector } from "react-redux";
+
+const BASE_URL=import.meta.env.MODE==="development" ? "http://localhost:1000/api/v1":"/api/v1";
 
 const Profile = () => {
 
@@ -75,7 +22,7 @@ const Profile = () => {
     const fetch = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:1000/api/v1/get-user-info",
+          `${BASE_URL}/get-user-info`,
           { headers }
         );
         setProfile(response.data);
